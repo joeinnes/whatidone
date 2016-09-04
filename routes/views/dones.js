e67ba3a73ad4a8e6776c0dc63601ba14c352d459
Joe Innes <joe@joeinn.es>
2016-09-04 20:28:38 +0200

    User only working

--- routes/views/dones.js
+++ routes/views/dones.js
@@ -1,11 +1,12 @@
 var keystone = require('keystone');
 var async = require('async');
+var moment = require('moment');
 
 exports = module.exports = function (req, res) {
 
   var view = new keystone.View(req, res);
   var locals = res.locals;
-
+  console.log(req.user._id);
   // Init locals
   locals.section = 'dones';
   /* locals.filters = {
@@ -17,17 +18,36 @@ exports = module.exports = function (req, res) {
 
   // Load the dones
   view.on('init', function (next) {
+    var end = moment().endOf('day')
+    var start = moment(end).subtract(5, 'days');
+    var q = keystone.list('Done')
+      .paginate({
+        page: req.query.page || 1,
+        perPage: 10,
+        maxPages: 10
+        /* filters: {
+          'createdBy.id': req.user._id
+        }*/
+      })
+      .populate('createdBy')
+      .sort('-doneType -completedOn');
 
-    var q = keystone.list('Done').paginate({
-      page: req.query.page || 1,
-      perPage: 10,
-      maxPages: 10
-      /* filters: {
-        state: 'published' // Date filter here?
-      }, */
-    })
-      .sort('-doneType -completedOn')
-      .populate('createdBy');
+    q.find({
+      $and: [{
+        createdBy: {
+          _id: req.user._id
+        }
+      }, {
+        $or: [{
+          completedOn: {
+            $gte: start,
+            $lt: end
+          }
+        }, {
+          completedOn: null
+        }]
+      }]
+    });
 
     q.exec(function (err, results) {
       locals.data.dones = results;
